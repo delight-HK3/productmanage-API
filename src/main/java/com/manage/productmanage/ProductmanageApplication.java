1f602e7cf617139009608be645f1f9161cb4b8e2
leedabean <dabin310153@naver.com>
2025-03-06 09:27:40 +0900

    springbootapplication file add

---
+++ src/main/java/com/manage/productmanage/ProductmanageApplication.java
@@ -0,0 +1,13 @@
+package com.manage.productmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmanageApplication.class, args);
+	}
+
+}
