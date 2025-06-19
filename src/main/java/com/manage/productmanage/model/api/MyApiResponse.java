6389dc55d59bfa1b350543d413b6c0737a7bb901
leebean <dabin310153@naver.com>
2025-06-19 14:41:09 +0900

    response class name change
    - ApiResponse -> MyApiResponse 이름변경
    - swagger ApiResonse 충돌 이유

--- src/main/java/com/manage/productmanage/model/api/MyApiResponse.java
+++ src/main/java/com/manage/productmanage/model/api/MyApiResponse.java
@@ -9,45 +9,45 @@ import lombok.Getter;
  * API 요청시 응답에 사용하는 객체
  */
 @Getter
-public class ApiResponse<T> {
+public class MyApiResponse<T> {
     private int code;
 
     private T data;         
     private String message; 
 
     // 조회에 성공 (리스트 리턴)
-    public ApiResponse(int code, T data, String message){
+    public MyApiResponse(int code, T data, String message){
         this.code = code;
         this.message = message;
         this.data = data;
     }
 
-    public ApiResponse(int code, String message){
+    public MyApiResponse(int code, String message){
         this.code = code;
         this.message = message;
     }
 
     // API 조회 결과 출력 (조회 결과 리턴)
-    public static <T> ResponseEntity<ApiResponse<T>> success(HttpStatusCode status, int code, T data, String message) {
+    public static <T> ResponseEntity<MyApiResponse<T>> success(HttpStatusCode status, int code, T data, String message) {
         return ResponseEntity.status(status)
-                             .body(new ApiResponse<T>(code,data,message));
+                             .body(new MyApiResponse<T>(code,data,message));
     }
 
     // API 조회 결과 출력 (생성,수정,삭제 성공)
-    public static <T> ResponseEntity<ApiResponse<T>> success(HttpStatusCode status, int code, String message) {
+    public static <T> ResponseEntity<MyApiResponse<T>> success(HttpStatusCode status, int code, String message) {
         return ResponseEntity.status(status)
-                             .body(new ApiResponse<T>(code,message));
+                             .body(new MyApiResponse<T>(code,message));
     }
     
     // API 조회 결과 출력 (조회 실패)
-    public static <T> ResponseEntity<ApiResponse<T>> fail(HttpStatusCode status, int code,String message) {
+    public static <T> ResponseEntity<MyApiResponse<T>> fail(HttpStatusCode status, int code,String message) {
         return ResponseEntity.status(status)
-                                 .body(new ApiResponse<T>(code,message));
+                                 .body(new MyApiResponse<T>(code,message));
     }
  
     // API 조회 결과 출력 (API 에러)
-    public static <T> ResponseEntity<ApiResponse<T>> error(HttpStatusCode status, int code,String message) {
+    public static <T> ResponseEntity<MyApiResponse<T>> error(HttpStatusCode status, int code,String message) {
         return ResponseEntity.status(status)
-                                 .body(new ApiResponse<T>(code,message));
+                                 .body(new MyApiResponse<T>(code,message));
     }
 }
